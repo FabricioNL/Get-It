6bf18eae4ae6877a4a0dcc652c61cf9226d44d5c
Fabricio2530 <fabricio.neri.ismart@gmail.com>
2021-09-23 16:26:12 -0300

    alterações

--- getit/getit/settings.py
+++ getit/getit/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'getit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'getit',
-        'USER': 'getituser',
-        'PASSWORD': 'getitsenha',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

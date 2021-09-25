067d9239774a6bd0569699481d525803f61dd9b1
Fabricio2530 <fabricio.neri.ismart@gmail.com>
2021-09-25 18:26:19 -0300

    alterando uma rota

--- notes/urls.py
+++ notes/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('delete', views.delete, name='delete'),
     path('update/save', views.save, name='editing'),
     path('tags', views.tags, name='all-tags'),
-    path('tags/Home', views.goHome, name="tag-home"), 
+    path('tags/Home', views.goHome, name="tag-home"),
+    path('/Home', views.goHome, name="tag-home"), 
     path('tags/<str:name>', views.tagVisualization, name="Tag-notes"),
 ]
